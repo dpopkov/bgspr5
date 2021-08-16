07f378d9eb42063e77de328011356c9f03e4555c
Denis Popkov <pkvdenis@gmail.com>
2021-08-16 14:36:42 +0300

    ch02: add XML application context for testing.

--- ch02/src/test/java/learn/bgspr5/ch02/GreeterTest.java
+++ ch02/src/test/java/learn/bgspr5/ch02/GreeterTest.java
@@ -1,9 +1,10 @@
 package learn.bgspr5.ch02;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.testng.annotations.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.testng.Assert.*;
@@ -12,12 +13,12 @@ public class GreeterTest {
 
     @Test
     public void testHelloWorld() {
-        Greeter greeter = new HelloWorldGreeter();
-        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try (PrintStream ps = new PrintStream(baos, true, StandardCharsets.UTF_8)) {
-            greeter.setPrintStream(ps);
-            greeter.greet();
-        }
+        ApplicationContext context = new ClassPathXmlApplicationContext("/applicationContext.xml");
+        Greeter greeter = context.getBean("helloGreeter", Greeter.class);
+        final ByteArrayOutputStream baos = context.getBean("baos", ByteArrayOutputStream.class);
+
+        greeter.greet();
+
         String actual = new String(baos.toByteArray(), StandardCharsets.UTF_8);
         assertEquals(actual, "Hello, World!");
     }
